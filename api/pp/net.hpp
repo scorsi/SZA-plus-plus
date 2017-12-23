87a818c8b92135d02997241f23a9123901afe9d5
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-12-23 12:54:58 +0100

    Net fromBasicNetInfo returns now a shared_ptr

--- api/pp/net.hpp
+++ api/pp/net.hpp
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <memory>
+
 #include "../net.h"
 
 
@@ -8,13 +10,15 @@ namespace zia::apipp {
 
     class Net {
     public:
-        static Net fromBasicNetInfo(zia::api::NetInfo &netInfo) {
-
+        static std::shared_ptr<Net> fromBasicNetInfo(zia::api::NetInfo &netInfo) {
+            return std::make_shared<Net>();
         }
 
-        zia::api::NetInfo toBasicNetInfo() {
+        zia::api::NetInfo toBasicNetInfo() const {
 
         }
     };
+
+    typedef std::shared_ptr<Net> NetPtr;
 
 }
