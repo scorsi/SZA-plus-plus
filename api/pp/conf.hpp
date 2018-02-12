a6e830e870fb018eaede3cbbaa82d82fb1143820
Anthony <anthony.leclerc@epitech.eu>
2018-02-12 01:21:49 +0100

    Fix compilation errors on Linux with g++-7:
    Error: Template specialisation in non namespace-scope.
    
    TODO: Test with MSVC

--- api/pp/conf.hpp
+++ api/pp/conf.hpp
@@ -69,34 +69,6 @@ namespace zia::apipp {
             }
         }
 
-        /**
-         * Alias for long long.
-         *
-         * @return
-         */
-        template<>
-        int get<int>() {
-            try {
-                return static_cast<int>(std::get<long long>(value));
-            } catch (std::bad_variant_access &) {
-                throw InvalidAccess();
-            }
-        }
-
-        /**
-         * Alias for double.
-         *
-         * @return
-         */
-        template<>
-        float get<float>() {
-            try {
-                return static_cast<float>(std::get<double>(value));
-            } catch (std::bad_variant_access &) {
-                throw InvalidAccess();
-            }
-        }
-
         ConfElem *get_at(const int index) {
             return (*this)[index];
         }
@@ -114,119 +86,6 @@ namespace zia::apipp {
          */
         template<typename T>
         ConfElem *set(T &&);
-
-        /**
-         * Set a ConfMap value.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<ConfMap *>(ConfMap *&&val) {
-            type = Map;
-            value = std::forward<ConfMap *>(val);
-            return this;
-        }
-
-        /**
-         * Set a ConfArray value.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<ConfArray *>(ConfArray *&&val) {
-            type = Array;
-            value = std::forward<ConfArray *>(val);
-            return this;
-        }
-
-        /**
-         * Set a std::string value.
-         * Alias for std::string.
-         *
-         * TODO: Fix following bug
-         * @bug when using like : conf.set("toto"). Possible fix: using the string_literal suffixe to use std::string instead of char *.
-         * @param val
-         */
-        template<>
-        ConfElem *set<char *>(char *&&val) {
-            type = String;
-            value = std::string(std::forward<char *>(val));
-            return this;
-        }
-
-        /**
-         * Set a std::string value.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<std::string>(std::string &&val) {
-            type = String;
-            value = std::forward<std::string>(val);
-            return this;
-        }
-
-        /**
-         * Set a long long value.
-         * Alias for long long.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<int>(int &&val) {
-            type = Integer;
-            value = static_cast<long long>(std::forward<int>(val));
-            return this;
-        }
-
-        /**
-         * Set a long long value.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<long long>(long long &&val) {
-            type = Integer;
-            value = std::forward<long long>(val);
-            return this;
-        }
-
-        /**
-         * Set a double value.
-         * Alias for double.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<float>(float &&val) {
-            type = Integer;
-            value = static_cast<double>(std::forward<float>(val));
-            return this;
-        }
-
-        /**
-         * Set a double value.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<double>(double &&val) {
-            type = Double;
-            value = std::forward<double>(val);
-            return this;
-        }
-
-        /**
-         * Set a bool value.
-         *
-         * @param val
-         */
-        template<>
-        ConfElem *set<bool>(bool &&val) {
-            type = Boolean;
-            value = std::forward<bool>(val);
-            return this;
-        }
 
         /**
          * Push a ConfElem into the ConfArray value.
@@ -314,6 +173,29 @@ namespace zia::apipp {
             return {};
         }
     };
+
+    /**
+     * Alias for long long.
+     *
+     * @return
+     */
+    template<>
+    inline int ConfElem::get<int>() {
+        return static_cast<int>(get<long long>());
+
+    }
+
+    /**
+     * Alias for double.
+     *
+     * @return
+     */
+    template<>
+    inline float ConfElem::get<float>() {
+
+        return static_cast<float>(get<double>());
+    }
+
 
     using Conf = ConfElem;
 }
